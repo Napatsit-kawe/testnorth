7bf53f445dc82a55d1f56d1c58c8471765f7ad55
Napatsit-kawe <123538436+Napatsit-kawe@users.noreply.github.com>
2023-06-06 15:06:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("โปรดป้อนจำนวนช่องกระเบื้องวัดจากด้านกว้าง (w):");
+        int width = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("โปรดป้อนจำนวนช่องกระเบื้องวัดจากด้านยาว (h):");
+        int height = int.Parse(Console.ReadLine());
+
+        int totalTiles = width * height;
+
+        if (totalTiles >= 5 && (width >= 6 || height >= 6 || width * height >= 26))
+        {
+            Console.WriteLine("Too stuffy!");
+        }
+        else
+        {
+            Console.WriteLine("Okay!");
+        }
+    }
+}
+
